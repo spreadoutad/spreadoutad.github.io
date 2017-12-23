1138c87199c059f4a056936b11808a00c5ce3188
Tony Lin <tony.lin@ardomusnet.com>
2017-12-24 01:20:26 +0800

    Update chinese i18n

--- js/main.js
+++ js/main.js
@@ -43,6 +43,115 @@ function main() {
         offset: 80
     })
 
+    var currentLanguage = 'zh';    
+
+    $('#aLang').on('click', function () {
+      console.log('lang');
+
+      if (currentLanguage === 'zh') {
+        currentLanguage = 'en';
+        updateText('en');
+      } else {
+        currentLanguage = 'zh';
+        updateText('zh');
+      }
+
+    });
+
+    var updateText = function (lang) {
+      if (lang === 'zh') {
+        $('#aHome').text('主頁');
+        $('#aAbout').text('關於我們');
+        $('#aServices').text('服務');
+        $('#aStats').text('數據');
+        $('#aTeam').text('團隊');
+        $('#aContact').text('聯絡我們');
+        $('#aLang').text('English');
+        $('#aLearnMore').text('知道更多');
+        $('#h2About').text('關於我們');
+        $('#h4WhoWeAre').text('我們是誰');
+        $('#pWhoWeAre').text('我們是一個由Louis新創的團隊，專注在行銷跟廣告');
+        $('#h4WhatWeDo').text('我們做什麼');
+        $('#pWhatWeDo').text('我們研發了一到廣告軟體系統，這套管理系統讓我們可以用非常低的成本達到比市面上的廣告更好的廣告效果');
+        $('#h4WhyChooseUs').text('為何選擇我們');
+        $('#pWhyChooseUs').text('如上面所說的，我們開發的軟體可以讓客戶已非常非常低的成本達到非常高的曝光量，我們會是您最好的選擇');
+        $('#h2OurServices').text('我們的服務');
+        $('#h4WebDevelopment').text('網頁開發');
+        $('#h4AppDevelopment').text('App 開發');
+        $('#h4Analystics').text('資料分析');
+        $('#h4Marketing').text('行銷規劃');
+        $('#h4eCommerce').text('網路行銷');
+        $('#h4ContentDevelopment').text('內容開發');
+        $('#h4Branding').text('品牌形象');
+        $('#h4VP').text('影片與照片');
+        $('#pWebDevelopment').text('我們可以替客戶打造專屬的拍賣官方網站，提供使用者絕佳的消費體驗．');
+        $('#pAppDevelopment').text('我們可以替客戶打造專屬的拍賣官方ＡＰＰ，提供使用者絕佳的消費體驗．');
+        $('#pAnalystics').text('我們可以替客戶分析他們所獲得的資料，讓客戶更暸解使用者，以增加更多利潤．');
+        $('#pMarketing').text('我們可以替客戶在網路上行銷他們的產品，增加客戶的銷售量，賣出比原本更多的商品．');
+        $('#peCommerce').text('我們可以整合現在市場上各大消費平台，提供更多的消費方式，讓消費更佳easy．');
+        $('#pContentDevelopment').text('我們可以替客戶開發產品的內容，內容會根據產品的種類和樣式有所不同．');
+        $('#pBranding').text('我們可以替客戶打造一個成功並且曝光量極高的品牌．');
+        $('#pVP').text('我們可以替客戶重新拍攝照片或者影片．');
+        $('#h2Statistic').text('統計數據');
+        $('#h4Impressions').text('曝光次數');
+        $('#pImpressions').text('所有貼文被查看的次數，從我們的統計數既顯示可以看出，曝光次數因為使用了我們的軟體成長的速度非常的快．');
+        $('#h4Reaches').text('觸及人數');
+        $('#pReaches').text('看過任何一則貼文的不重複帳號數量，從我們的統計數既顯示可以看出，觸及人數因為使用了我們的軟體成長的速度非常的快．');
+        $('#h4ProfileViews').text('個人檔案瀏覽次數');
+        $('#pProfileViews').text('專頁檔案被瀏覽的次數，從我們的統計數既顯示可以看出，個人檔案瀏覽次數因為使用了我們的軟體成長的速度非常的快．');
+        $('#h2Team').text('團隊成員');
+        $('#h2Contact').text('聯絡我們');
+        $('#h4ContactInfo').text('聯絡資訊');
+        $('#h4LeaveMessage').text('留下訊息');
+        $('#btnSend').text('送出訊息');
+      } else {
+        $('#aHome').text('Home');
+        $('#aAbout').text('About');
+        $('#aServices').text('Services');
+        $('#aStats').text('Statistics');
+        $('#aTeam').text('Team');
+        $('#aContact').text('Contact');
+        $('#aLang').text('中文');
+        $('#aLearnMore').text('Learn More');
+        $('#h2About').text('About Us');
+        $('#h4WhoWeAre').text('Who We Are');
+        $('#pWhoWeAre').text('We are a team newly founded by Louis, and we are focusing on Marketing and Advertising.');
+        $('#h4WhatWeDo').text('What We Do');
+        $('#pWhatWeDo').text('We developed a marketing advertising software system which leads to much more advertising benifit the other advertising method, and the cost of this is very low.');
+        $('#h4WhyChooseUs').text('Why Choose Us');
+        $('#pWhyChooseUs').text('As described, the  we developed can produce a very large amount of exposure of your product at a very low price, we\'re the best choice.');
+        $('#h2OurServices').text('Our Services');
+        $('#h4WebDevelopment').text('Web Development');
+        $('#h4AppDevelopment').text('App Development');
+        $('#h4Analystics').text('Analystics');
+        $('#h4Marketing').text('Marketing');
+        $('#h4eCommerce').text('eCommerce');
+        $('#h4ContentDevelopment').text('Content Development');
+        $('#h4Branding').text('Branding');
+        $('#h4VP').text('Video & Photography');
+        $('#pWebDevelopment').text('We develop shopping website for our customers to let their customers have the greatest shopping experience using computer.');
+        $('#pAppDevelopment').text('We develop shopping app for Android an IOS for our customers to let their customers have the greatest shopping experience using app.');
+        $('#pAnalystics').text('We can help our customer analyze their data and make the best profit.');
+        $('#pMarketing').text('We use our advertising software system to form a best marketing strategy.');
+        $('#peCommerce').text('We integerate most popular shopping method for our customer to make shopping easier.');
+        $('#pContentDevelopment').text('We develop the content of products the customers gave us.');
+        $('#pBranding').text('We help our customers build a successful and maximum exposure brand which leads to success.');
+        $('#pVP').text('We can re-shoot the photo and video for our customer.');
+        $('#h2Statistic').text('Statistics');
+        $('#h4Impressions').text('Impressions');
+        $('#pImpressions').text('The total number of times all of your posts have been seen. From the statistic graph you can the impression number is growing very fast using our advertising software system.');
+        $('#h4Reaches').text('Reaches');
+        $('#pReaches').text('The number of unique accounts that have seen any of your posts. From the statistic graph you can the reaches number is growing very fast using our advertising software system.');
+        $('#h4ProfileViews').text('Profile Views');
+        $('#pProfileViews').text('The number of time your profile was viewed. From the statistic graph you can the profile view number is growing very fast using our advertising software system.');
+        $('#h2Team').text('Meet the Team');
+        $('#h2Contact').text('Contact Us');
+        $('#h4ContactInfo').text('Contact info');
+        $('#h4LeaveMessage').text('Leave us a message');
+        $('#btnSend').text('Send Message');
+      }
+    };    
+
 
   	/*====================================
     Portfolio Isotope Filter
